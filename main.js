7abefd99f189bf7454b307d061c8e558f5ca7f70
Kian <71701590+Kian-Code@users.noreply.github.com>
2020-12-10 19:45:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+Nose_X = 0;
+Nose_Y = 0;
+Left_Wrist = 0;
+Right_Wrist = 0;
+Diffrence = 0; 
+
+function setup()
+{
+    Video = createCapture(VIDEO);
+    Video.size(550 , 500);
+    Canvas = createCanvas(550 , 500);
+    Canvas.position(560 , 120);
+    PoseNet = ml5.poseNet(Video , ModelLoaded);
+    PoseNet.on('pose' , GotPoses);
+}
+
+function ModelLoaded()
+{
+console.log("PoseNet is initialized");
+}
+
+function draw()
+{
+    background('#499F68');
+    document.getElementById("Square_Sides").innerHTML = "Size of the text will be: " + Diffrence + "px";
+    textSize(Diffrence);
+    fill('#79A9D1');
+    text('Kian!' , Nose_X , Nose_Y);
+
+}
+
+function GotPoses(results)
+{
+if (results.length > 0) {
+    console.log(results);
+Nose_X = results[0].pose.nose.x;
+Nose_Y = results[0].pose.nose.y;
+console.log("Nose X = " + Nose_X + "Nose Y = " + Nose_Y);
+Left_Wrist = results[0].pose.leftWrist.x;
+Right_Wrist = results[0].pose.rightWrist.x;
+console.log("Left Wrist X = " + Left_Wrist + "Right Wrist X = " + Right_Wrist);
+Diffrence = floor(Left_Wrist - Right_Wrist);
+}
+}
